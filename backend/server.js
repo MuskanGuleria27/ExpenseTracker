f7a1b4d4d86786b753e4b49d9a03f92e1222c0cd
Abhiraj446 <abhirajkhatana0@gmail.com>
2026-06-15 14:28:26 +0530

    fixed cors issue

--- backend/server.js
+++ backend/server.js
@@ -16,10 +16,15 @@ const dashboardRoutes = require('./routes/dashboardRoutes');
 const app = express();
 
 app.use(cors({
-  origin: process.env.CLIENT_URL||'*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  origin: [
+    "https://expensetracker-six-rho.vercel.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
+
+app.options("*", cors());
 
 app.use(express.json());
 
@@ -27,10 +32,10 @@ connectDB();
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-app.use("/api/v1/auth",authRoutes);
-app.use("/api/v1/income",incomeRoutes);
-app.use("/api/v1/expense",expenseRoutes);
-app.use("/api/v1/dashboard",dashboardRoutes);
+app.use("https://expensetracker-ls4.onrender.com/api/v1/auth",authRoutes);
+app.use("https://expensetracker-ls4.onrender.com/api/v1/income",incomeRoutes);
+app.use("https://expensetracker-ls4.onrender.com/api/v1/expense",expenseRoutes);
+app.use("https://expensetracker-ls4.onrender.com/api/v1/dashboard",dashboardRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
